d675a719ababaad935be2845d553e77ec50fae1f
samdao39 <154991524+samdao39@users.noreply.github.com>
2024-03-11 21:41:14 +0700

    bai_tap_Locators

--- src/test/java/Bai5_Locators/LocatorsCRM.java
+++ src/test/java/Bai5_Locators/LocatorsCRM.java
@@ -1,14 +1,14 @@
 public class LocatorsCRM {
 String email_Address = "//input[@id='email']"  ;
- String Password ="//input[@type='password']" ;
- String remember_Me= "//label[normalize-space()='Remember me']";
- String btn_Login="//button[normalize-space()='Login']";
- String forgot_Password="a[href='https://crm.anhtester.com/admin/authentication/forgot_password']";
+String password ="//input[@type='password']" ;
+String remember_Me= "//label[normalize-space()='Remember me']";
+String btn_Login="//button[normalize-space()='Login']";
+String forgot_Password="a[href='https://crm.anhtester.com/admin/authentication/forgot_password']";
 String btn_Customers="//span[normalize-space()='Customers']";
 String btn_NewCustomer="//a[normalize-space()='New Customer']";
 String tab_CustomerDetails= "//a[normalize-space()='Customer Details']";
 
- String tab_Billing_Shipping ="//a[normalize-space()='Billing & Shipping']"/ "//a[contains(normalize-space(),'Billing & Shipping')]"/ "//a[starts-with(normalize-space(),'Billing')]"
+String tab_Billing_Shipping ="//a[normalize-space()='Billing & Shipping']"// "//a[contains(normalize-space(),'Billing & Shipping')]"/ "//a[starts-with(normalize-space(),'Billing')]"
  String Company = "//input[@id='company']";
  String search = "//input[@id='search_input']";
  String vat_Number = "//input[@id='vat']";
@@ -19,9 +19,9 @@ String tab_CustomerDetails= "//a[normalize-space()='Customer Details']";
 String address ="//textarea[@id='address']";
 
 String city ="//input[@id='city']";
- String state ="//input[@id='state']";
- String zip ="//input[@id='zip']";
- String btn_save="//button[@class='btn btn-primary only-save customer-form-submiter']";/(//button[normalize-space()='Save'])[2]
+String state ="//input[@id='state']";
+String zip ="//input[@id='zip']";
+String btn_save="//button[@class='btn btn-primary only-save customer-form-submiter']";/(//button[normalize-space()='Save'])[2]
  String btn_saveAndCreateContact="//button[normalize-space()='Save and create contact']";
 
 
@@ -29,7 +29,7 @@ String city ="//input[@id='city']";
  String btn_newProject="//a[@href='https://crm.anhtester.com/admin/projects/gantt']/preceding-sibling::a";
 
  String projectName_textbox="//label[@for='name']/following-sibling::input";
- String customer_combobox="//select[@id='clientid']/ancestor::div[1]";
+ String customer_Combobox="//select[@id='clientid']/ancestor::div[1]";
  String typeToSearch_textbox="///div[@id='bs-select-6']/preceding-sibling::div";
  String calculate_Checkbock="//input[@id='progress_from_tasks']";
  String billingType="//button[@data-id='billing_type']";
@@ -42,7 +42,7 @@ String city ="//input[@id='city']";
  String tags="//input[@placeholder='Tag']/ancestor::li";
  String description = "(//iframe[@id='description_ifr'])[1]";
  String sendCheckbox="//input[@id='send_created_email']";
- String save_button ="(//input[@id='send_created_email'])[1]";
+ String save_Button ="(//input[@id='send_created_email'])[1]";
 
 
 
